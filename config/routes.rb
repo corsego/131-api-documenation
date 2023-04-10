037383ba67664e3a261518f9b6e7a4df2fdcd461
yshmarov <yshmarov@gmail.com>
2023-04-10 18:10:23 +0200

    update static pages urls and pass tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/dashboard'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#landing_page'
+  get 'dashboard', to: 'static_pages#dashboard'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get landing_page" do
-    get static_pages_landing_page_url
+    get root_url
     assert_response :success
+    assert_match 'StaticPages#landing_page', response.body
   end
 
   test "should get dashboard" do
-    get static_pages_dashboard_url
+    get dashboard_url
     assert_response :success
+    assert_match 'StaticPages#dashboard', response.body
   end
 end
